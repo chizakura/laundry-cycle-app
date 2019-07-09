e1c74444b44c59de2213976ed9bc84217e1a78b2
Linda Chin <lindapchin225@gmail.com>
2019-07-08 23:00:33 -0400

    added basic components

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,33 @@
-import React from 'react';
-import './App.css';
+import React, {Component} from 'react';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import {Switch, Route} from 'react-router-dom';
+import Home from './Components/Home';
+import Login from './Components/Login';
+import SignUpForm from './Components/SignUpForm';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Laundry Cycle</h1>
-    </div>
-  )
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Navbar bg="light" variant="light">
+          <Navbar.Brand href="/">Laundry Cycle</Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav"/>
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="justify-content-center">
+              <Nav.Item><Nav.Link href="/login">Login</Nav.Link></Nav.Item>
+              <Nav.Item><Nav.Link href="/signup">Sign Up</Nav.Link></Nav.Item>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+        <Switch>
+          <Route exact path="/" component={Home}/>
+          <Route exact path="/login" component={Login}/>
+          <Route exact path="/signup" component={SignUpForm}/>
+        </Switch>
+      </div>
+    )
+  }
 }
 
 export default App;
---
+++ client/src/Components/Home.jsx
@@ -0,0 +1,23 @@
+import React, {Component} from 'react';
+import Figure from 'react-bootstrap/Figure';
+
+class Home extends Component {
+    render() {
+        return (
+            <div>
+                {/* <Image src="https://campuslodgegainesville.com/wp-content/uploads/sites/68/2018/02/washer-dryer-in-every-unit.gif" alt="laundry-logo" fluid/> */}
+                <Figure>
+                <Figure.Caption className="caption-color">
+                    We can only transform our lives if we sincerely want to.  Small changes transform our lives. --Marie Kondo
+                </Figure.Caption>
+                <Figure.Image
+                    alt="laundry-logo"
+                    src="https://campuslodgegainesville.com/wp-content/uploads/sites/68/2018/02/washer-dryer-in-every-unit.gif"
+                />
+                </Figure>
+            </div>
+        )
+    }
+}
+
+export default Home;
