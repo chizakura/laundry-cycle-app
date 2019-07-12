c7b44075cce6ab644cd6ba1d34a343f42b7295b5
Linda Chin <lindapchin225@gmail.com>
2019-07-12 13:12:35 -0400

    added toggle ability for options

--- client/src/Components/ShowClothingItem.jsx
+++ client/src/Components/ShowClothingItem.jsx
@@ -4,12 +4,13 @@ import {Link} from 'react-router-dom';
 import Card from 'react-bootstrap/Card';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import Badge from 'react-bootstrap/Badge'
+import Badge from 'react-bootstrap/Badge';
+import Accordion from 'react-bootstrap/Accordion';
 
 class ShowClothingItem extends Component {
     componentDidMount() {
         const {handleClothingItem} = this.props;
-        handleClothingItem(this.props.match.params.itemId)
+        handleClothingItem(this.props.match.params.itemId);
     }
 
     editButton = () => {
@@ -27,7 +28,7 @@ class ShowClothingItem extends Component {
                 <Card>
                     <Card.Header className="item-header">
                         <Card.Link as={Link} to="/closet" className="back-button"><i className="material-icons item-button">navigate_before</i>Closet</Card.Link>
-                        <h3 className="item-title">{clothingItem.name}</h3>
+                        <h3 className="item-title">{clothingItem.description}</h3>
                         <Badge><i onClick={this.deleteButton} className="material-icons item-button">delete</i></Badge>
                     </Card.Header>
                     <Card.Body>
@@ -35,18 +36,48 @@ class ShowClothingItem extends Component {
                             <Col><Card.Img className="profile-pic" src={shirt}/></Col>
                             <Col>
                                 <Card.Text className="text-left profile-text">
+                                    Shade Category: {clothingItem.colorShade}
+                                    <Badge size="sm"><i onClick={this.editButton} className="material-icons item-button">edit</i></Badge>
+                                </Card.Text>
+                                <Card.Text className="text-left profile-text">
                                     Type: {clothingItem.type}
                                     <Badge size="sm"><i onClick={this.editButton} className="material-icons item-button">edit</i></Badge>
                                 </Card.Text>
                                 <Card.Text className="text-left profile-text">
-                                    Color: {clothingItem.color}
+                                    Material: {clothingItem.material}
+                                    <Badge size="sm"><i onClick={this.editButton} className="material-icons item-button">edit</i></Badge>
+                                </Card.Text>
+                                <Card.Text className="text-left profile-text">
+                                    Brand: {clothingItem.brand}
                                     <Badge size="sm"><i onClick={this.editButton} className="material-icons item-button">edit</i></Badge>
                                 </Card.Text>
                             </Col>
                         </Row>
                         <Row>
-                            <Col><Card.Header>Washer Options</Card.Header></Col>
-                            <Col><Card.Header>Dryer Options</Card.Header></Col>
+                            <Col>
+                                <Accordion>
+                                    <Card bg="info" text="white">
+                                        <Accordion.Toggle as={Card.Header} eventKey="0">Washer Option</Accordion.Toggle>
+                                        <Accordion.Collapse eventKey="0">
+                                            <Card.Body>
+                                                <Card.Text>{clothingItem.washoptionId}</Card.Text>
+                                            </Card.Body>
+                                        </Accordion.Collapse>
+                                    </Card>
+                                </Accordion>
+                            </Col>
+                            <Col>
+                                <Accordion>
+                                    <Card bg="info" text="white">
+                                        <Accordion.Toggle as={Card.Header} eventKey="0">Dry Option</Accordion.Toggle>
+                                        <Accordion.Collapse eventKey="0">
+                                            <Card.Body>
+                                                <Card.Text>{clothingItem.dryoptionId}</Card.Text>
+                                            </Card.Body>
+                                        </Accordion.Collapse>
+                                    </Card>
+                                </Accordion>
+                            </Col>
                         </Row>
                     </Card.Body>
                 </Card>
