158e320738a5d32fcc79ade5e693ecee826839dd
Linda Chin <lindapchin225@gmail.com>
2019-07-15 07:39:43 -0400

    included styles.css

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-// import './Washicons-master/washicons/styles.css';
+import './styles.css';
 import App from './App';
 import {BrowserRouter as Router} from 'react-router-dom';
 
