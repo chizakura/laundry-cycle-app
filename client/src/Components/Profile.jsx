e7594857f2b4c1a9d9543f7c4248de2e3fcd4f1a
Linda Chin <lindapchin225@gmail.com>
2019-07-27 16:22:44 -0400

    working on uploading profile pic

--- client/src/Components/Profile.jsx
+++ client/src/Components/Profile.jsx
@@ -5,11 +5,24 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
 class Profile extends Component {
+    // state = {
+    //     avatar: ""
+    // }
+
     componentDidMount(){
         const {handleClothes} = this.props;
         const userId = localStorage.getItem('userId');
         handleClothes(userId)
     }
+
+    // handleUpload = (event) => {
+    //     const {name, value} = event.target;
+    //     // const objectUrl = URL.createObjectURL(value);
+    //     // console.log(objectUrl)
+    //     this.setState({
+    //         [name]: value
+    //     })
+    // }
 
     render() {
         const {user, count} = this.props;
@@ -25,6 +38,13 @@ class Profile extends Component {
                             </Col>
                             <Col xs={12} md={6}>
                                 <Card.Text className="text-left profile-text">Number of clothing items: {count}</Card.Text>
+                                {/* <form>
+                                    <label>Choose a profile picture:</label>
+                                    <br></br>
+                                    <input type="file" name="avatar" accept="image/png, image/jpeg" value={this.state.avatar} onChange={this.handleUpload}></input>
+                                    <br></br>
+                                    <input type="submit"/>
+                                </form> */}
                             </Col>
                         </Row>
                     </Card.Body>
